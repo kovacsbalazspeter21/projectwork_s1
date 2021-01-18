e2cad64feaa460209deb8d199298d9dce4ed6565
kovacsbalazspeter21 <71692659+kovacsbalazspeter21@users.noreply.github.com>
2021-01-18 14:56:16 +0100

    Add files via upload

---
+++ work1_projectwork.py
@@ -0,0 +1,49 @@
+import turtle
+t = turtle.Turtle()
+t.hideturtle()
+t.pendown()
+for x in range(30):
+    t.color("red")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
+for x in range(30):
+    t.color("blue")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
+for x in range(30):
+    t.color("green")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
+for x in range(30):
+    t.color("brown")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
+for x in range(30):
+    t.color("purple")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
+for x in range(30):
+    t.color("orange")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
+for x in range(30):
+    t.color("black")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
+for x in range(30):
+    t.color("blue")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
+for x in range(30):
+    t.color("red")
+    t.circle(90, 80)
+    if x % 2 == 0:
+        t.circle(20, 70)
